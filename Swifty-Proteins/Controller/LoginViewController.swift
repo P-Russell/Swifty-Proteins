b6653395888014c01fb6228e1de2c40120878795
Daniel BARROW <dbarrow@e4r6p7.wethinkcode.co.za>
2018-11-07 14:08:06 +0200

    login working and loginview opened from background

--- Swifty-Proteins/Controller/LoginViewController.swift
+++ Swifty-Proteins/Controller/LoginViewController.swift
@@ -7,32 +7,82 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var invalidLabel: UILabel!
+    @IBOutlet weak var loginField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    func showAlertController(_ message: String) {
+        let alertController = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+    
     @IBAction func loginButton(_ sender: UIButton) {
-        moveToTable()
+        if (loginField.text == "dbarrow" && passwordField.text == "password") {
+            self.performSegue(withIdentifier: "loginSegue", sender: nil)
+        }
+        else {
+            loginField.text = ""
+            passwordField.text = ""
+            invalidLabel.isHidden = false
+        }
+    }
+    @IBAction func touchID(_ sender: UIButton) {
+        // 1
+        let context = LAContext()
+        var error: NSError?
+        
+        // 2
+        // check if Touch ID is available
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            // 3
+            let reason = "Authenticate with Touch ID"
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason, reply:
+                {(success, error) in
+                    // 4
+                    if success {
+                        self.performSegue(withIdentifier: "loginSegue", sender: nil)
+                        self.showAlertController("Touch ID Authentication Succeeded")
+                    }
+                    else {
+                        self.showAlertController("Touch ID Authentication Failed")
+                    }
+                    } as (Bool, Error?) -> Void)
+        }
+            // 5
+        else {
+            showAlertController("Touch ID not available")
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.loginButton.isHidden = true
+        let context = LAContext()
+        var error: NSError?
+        
+        // 2
+        // check if Touch ID is available
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            // 3
+            self.loginButton.isHidden = false
+        }
         // Do any additional setup after loading the view.
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        moveToTable()
+        //moveToTable()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    func moveToTable() {
-        let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
-        let newViewController = storyBoard.instantiateViewController(withIdentifier: "NavigationController") as! UINavigationController
-        self.present(newViewController, animated: true, completion: nil)
-    }
+
     
 
     /*
